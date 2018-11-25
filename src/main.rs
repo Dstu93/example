07c5902c00edaec08ed7f4f014b0b7ff12ba278b
doenis <Dstu93@users.noreply.github.com>
2018-11-25 11:45:18 +0100

    fixed test code in the main fn

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use frontend::syntax::*;
 use frontend::lexer::Lexer;
 
 fn main() {
-    let src = " let s = 5; \n let b = a;";
+    let src = " let s = 5; \n let b = a;".into();
     println!("{}",&src);
-    let res = Lexer::tokenize(&src);
+    let res = Lexer::tokenize(src).0.collect();
     println!("{:#?}",res);
 }
