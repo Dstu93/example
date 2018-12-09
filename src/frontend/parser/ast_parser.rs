e844049db69399ff06901e1479a2ceb500401343
dstu <dstu93@noreply-github.com>
2018-12-09 13:46:17 +0100

    some parsing stuff

--- src/frontend/parser/ast_parser.rs
+++ src/frontend/parser/ast_parser.rs
@@ -1,15 +1,18 @@
 use frontend::syntax::{DataType,DataValue,token::*,ast::*};
 
-pub struct ASTParser;
+pub struct ASTParser{
+    stack: Vec<Token>,
+    stream: TokenStream,
+}
 
 impl ASTParser{
 
-    pub fn new() -> Self{
-        ASTParser{}
+    pub fn new(stream: TokenStream) -> Self{
+        ASTParser{stack: Vec::with_capacity(1), stream}
     }
 
     // mut is not currently necessary because we do not mutate inner state
-    pub fn parse(self, stream: TokenStream) -> Result<AbstractSyntaxTree,ParseError>{
+    pub fn parse(self) -> Result<AbstractSyntaxTree,ParseError>{
 
         //vom stream lesen bis }
         //Vec an Tokens an unterfunktion geben zum parsen
@@ -18,27 +21,48 @@ impl ASTParser{
 
         // we split the TokenStream in parts, in functions as small units
         // se we can parse them multiple threads
-        let mut ob = 0; //number of open brackets {
-        let mut cb = 0; //number of closed brackets }
+        let fn_unit = ASTParser::separate_function(stream);
 
-        let mut fn_unit = Vec::with_capacity(20);
-        loop {
-            match stream.next() {
-                Some(t) => {
-                    if t.kind() == TokenType::SeparatorCurvedBracketOpen { ob += 1; }
-                    else if t.kind() == TokenType::SeparatorCurvedBracketClosed{ cb += 1; }
-                    fn_unit.push(t);
-                    // if we closed a scope {} we break
-                    if ob > 0 && cb > 0 && ob == cb { break; }
-                },
-                None => break
-            };
-        }
         println!("fn unit: {:#?}",fn_unit);
 
         Err(ParseError::Unknown)
     }
 
+    fn separate_function(stream: TokenStream) -> Result<Vec<Token>,ParseError> {
+        let mut ob = 0;
+        //number of open brackets {
+        let mut cb = 0;
+        //number of closed brackets }
+        let mut fn_unit = Vec::with_capacity(20);
+        loop {
+            match stream.next() {
+                Some(t) => {
+                    if t.kind() == TokenType::SeparatorCurvedBracketOpen { ob += 1; } else if t.kind() == TokenType::SeparatorCurvedBracketClosed { cb += 1; }
+                    fn_unit.push(t);
+                    // if we closed a scope {} we break
+                    if p_balanced(ob, cb) { break; }
+                },
+                None => {
+                    //check if parenthesis are balanced
+                    if !p_balanced(ob, cb) {
+                        let kind = if ob > cb {
+                            TokenType::SeparatorCurvedBracketClosed
+                        } else {
+                            TokenType::SeparatorCurvedBracketOpen
+                        };
+                        return Err(ParseError::Missing(kind));
+                    }
+                    break;
+                }
+            };
+        }
+        Ok(fn_unit)
+    }
+}
+
+/// checks if parenthesis are balanced
+fn p_balanced(ob: usize, cb: usize) -> bool{
+    ob > 0 && cb > 0 && ob == cb
 }
 
 
@@ -48,7 +72,8 @@ pub enum ParseError{
     /// This Number token cant get parsed to a number, its invalid
     NaN(Token),
     /// Found, Expected
-    WrongToken(Token,Token),
+    WrongToken(Token,TokenType),
     /// This Token is not allowed outside of a function
     OutOfFnScope(Token),
+    Missing(TokenType),
 }
--- src/frontend/syntax/token.rs
+++ src/frontend/syntax/token.rs
@@ -91,22 +91,26 @@ impl Token {
 /// Stream of Tokens.
 pub struct TokenStream {
     rx: Receiver<Token>,
+    is_closed: bool,
 }
 
 impl TokenStream{
 
     /// creates a new empty stream with a receiver to fill this stream
     pub fn new(rx: Receiver<Token>) -> Self{
-        TokenStream{rx}
+        TokenStream{rx,is_closed: false}
     }
 
     /// read next token from this stream and blocks the calling thread till a token is received.
     /// otherwise it will returns None if the stream closed and will never send a next token.
     /// The last token is always an EOF-Token, except the producer fails
-    pub fn next(&self) -> Option<Token>{
+    pub fn next(&mut self) -> Option<Token>{
         match self.rx.recv() {
             Ok(t) => Some(t),
-            Err(_) => None,
+            Err(_) => {
+                self.is_closed = true;
+                None
+            },
         }
     }
 
@@ -114,6 +118,11 @@ impl TokenStream{
     /// this function blocks the calling thread till every token is received
     pub fn collect(self) -> Vec<Token>{
         self.rx.iter().collect()
+    }
+
+    /// returns a boolean if the last token was
+    pub fn is_closed(&self) -> bool{
+        self.is_closed
     }
 
 }
