277376c1556715c594b2d2d5a3effe59fe050781
doenis <Dstu93@users.noreply.github.com>
2019-01-18 22:08:57 +0100

    added new testcase for AstParser

--- src/tests/ast_parser_test.rs
+++ src/tests/ast_parser_test.rs
@@ -53,7 +53,7 @@ fn function_with_return_type_test(){
     let ast = ASTParser::new(ts).parse().expect("expected abstract syntax tree");
 
     let return_str = Expression::new(NodeId::new(6),ExpressionKind::Literal(DataValue::String("a b c d e f g".into())));
-    let return_expr = Expression::new(NodeId::new(5),ExpressionKind::Return(Box::new(Some(return_str))));
+    let return_expr = Expression::new(NodeId::new(5),ExpressionKind::Return(Some(Box::new(return_str))));
     let return_stmt = Statement::new(NodeId::new(4),StatementKind::Expression(return_expr));
     let block = Block::new(NodeId::new(3),vec![return_stmt]);
     let test_fn_decl = StatementKind::Expression(Expression::new(NodeId::new(2),ExpressionKind::FnDecl("test".into(),block,None,Some(DataType::String))));
@@ -61,4 +61,31 @@ fn function_with_return_type_test(){
 
     let expected_ast = AbstractSyntaxTree::new(vec![test_fn]);
     assert_eq!(expected_ast,ast, "We are comparing two ast build from this source: {}",src);
+}
+
+//FIXME Return(Option<Box<Expression>>) && Statement(Expression) without NodeId
+
+#[test]
+fn function_with_arguments() {
+    let src = String::from("\
+    fn calculate(x: integer, y: integer): integer {\
+        let solution: integer = x * y;\
+        return solution;\
+    }");
+    println!("src: {}",&src);
+    let (ts,handle) = Lexer::tokenize(src);
+    let ast = ASTParser::new(ts).parse().expect("Expected Abstract Syntax Tree");
+
+    let return_value = Expression::new(10.into(),ExpressionKind::Symbol(2.into()));
+    let return_expression = Expression::new(9.into(),ExpressionKind::Return(Some(Box::new(return_value))));
+    let return_statement = Statement::new(8.into(),StatementKind::Expression(return_expression));
+    let y = Expression::new(7.into(),ExpressionKind::Symbol(1.into()));
+    let x = Expression::new(6.into(),ExpressionKind::Symbol(0.into()));
+    let multiplication = Expression::new(5.into(),ExpressionKind::BinaryOp(BinOp::Multi,Box::new(x),Box::new(y)));
+    let solution_binding = VariableBinding::new(4.into(),DataType::Integer,2.into());
+    let assignment = Statement::new(3.into(),StatementKind::Declaration(solution_binding,multiplication));
+    let args = vec![DataType::Integer,DataType::Integer];
+    let test_fn_block = Block::new(2.into(),vec![assignment,return_statement]);
+    let fn_declaration = Expression::new(1.into(),ExpressionKind::FnDecl("calculate".into(),test_fn_block,Some(args),Some(DataType::Integer)));
+    let test_fn = Statement::new(0.into(),StatementKind::Expression(fn_declaration));
 }
