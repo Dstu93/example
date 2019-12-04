c16bfebcca2b15175565bc3721c357434a7c5e1d
doenis <Dstu93@users.noreply.github.com>
2019-12-04 12:34:35 +0100

    defined some parts of the interpret. Fails at the borrow checker

--- src/backend/interpreter.rs
+++ src/backend/interpreter.rs
@@ -1,30 +1,75 @@
-use crate::frontend::syntax::ast::AbstractSyntaxTree;
+use crate::frontend::syntax::ast::{AbstractSyntaxTree, Statement, VariableBinding, Block};
 use crate::backend::memory::MemUnit;
-use crate::frontend::syntax::DataValue;
+use crate::frontend::syntax::{DataValue, DataType};
+use std::collections::HashMap;
+use crate::backend::interpreter::RuntimeError::ExpectedFnDeclaration;
 
 /// Takes an AbstractSyntaxTree and executes it at runtime.
 pub struct RuntimeInterpreter<'a,T> where T: MemUnit<DataValue> {
     ast: &'a AbstractSyntaxTree,
     heap: T,
+    fn_map: HashMap<&'a str,Function<'a>>,
 }
 
 impl <'a,T>RuntimeInterpreter<'a,T> where T: MemUnit<DataValue> {
 
     /// creates a new RuntimeInterpreter Object with an AbstractSyntaxTree to execute
     pub fn new(ast: &'a AbstractSyntaxTree, mem_unit: T) -> Self {
-        RuntimeInterpreter{ast, heap: mem_unit}
+        RuntimeInterpreter{
+            ast,
+            heap: mem_unit,
+            fn_map: HashMap::new()
+        }
     }
 
     /// starts the RuntimeInterpreter and executes the program described by the AbstractSyntaxTree.
     /// This method is self consuming and the interpreter only stops after completed the execution or
     /// a RuntimeError occurs
-    pub fn start(self) -> Result<(),RuntimeError> {
-        //TODO
-        Err(RuntimeError::NullPointerError)
+    pub fn start(&mut self) -> Result<(),RuntimeError> {
+        self.add_fn_declarations()?;
+        self.execute_main()
+    }
+
+    fn add_fn_declarations(&mut self) -> Result<(),RuntimeError> {
+        for node in &self.ast.nodes {
+            match node {
+                Statement::FnDecl(name, block, args, return_type) => {
+                    let function = Function { name, args, return_type, body: block };
+                    self.fn_map.insert(name, function);
+                },
+                _ => return Err(ExpectedFnDeclaration),
+            }
+        }
+        Ok(())
+    }
+
+    fn execute_main(&mut self) -> Result<(),RuntimeError> {
+        let main_func = match self.fn_map.get_mut("main") {
+            Some(func) => func,
+            None => return Err(RuntimeError::NoMainFn),
+        };
+        self.execute_function(main_func)
+    }
+
+    fn execute_function(&mut self, func: &Function) -> Result<(),RuntimeError> {
+        Ok(())
     }
 }
 
-#[derive(Ord, PartialOrd, Eq, PartialEq,Copy, Clone,Hash,Debug)]
+#[derive(PartialOrd,PartialEq,Clone,Debug)]
 pub enum RuntimeError {
+    /// Error when Function declaration is expected,
+    /// like the first statement in the AST
+    ExpectedFnDeclaration,
+    /// no main function found
+    NoMainFn,
     NullPointerError
+}
+
+/// wrapper for a function declaration
+struct Function<'a> {
+    pub name: &'a String,
+    pub args: &'a Vec<VariableBinding>,
+    pub return_type: &'a Option<DataType>,
+    pub body: &'a Block,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use frontend::lexer::Lexer;
 use frontend::parser::ast_parser::*;
+use crate::backend::interpreter::RuntimeInterpreter;
+use crate::backend::memory::Heap;
+use crate::frontend::syntax::DataValue;
 
 mod frontend;
 mod backend;
@@ -20,5 +23,24 @@ fn main() {
     let (ts,_) = Lexer::tokenize(src.clone());
     let parser = ASTParser::new(ts);
     let result  = parser.parse();
-    println!("Lexer Result: {:#?}", result);
+    let ast = match result {
+        Ok(ast) => {ast},
+        Err(error) => {
+            println!("Could not parse program: {:#?}", error);
+            return;
+        },
+    };
+
+    //execute program
+    let calculated_heap_size = 512 * 1024 * 1024 / std::mem::size_of::<DataValue>();
+    println!("max heap size: {}", calculated_heap_size);
+    let heap_space = Heap::new(calculated_heap_size);
+    let mut interpreter = RuntimeInterpreter::new(&ast,heap_space);
+    let execution_result = interpreter.start();
+    match execution_result {
+        Ok(_) => {},
+        Err(error) => {
+            println!("RuntimeError {:#?}", error);
+        },
+    }
 }
